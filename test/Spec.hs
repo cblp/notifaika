6eebe68d5f9a7331c7096d21019ead5ca3b1555a
Yuriy Syrovetskiy <cblp@cblp.su>
2015-09-17 19:10:13 +0300

    [+] TestIO?

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,8 +1,22 @@
+{-# LANGUAGE DeriveFunctor #-}
+
+import Lib
+
 import Test.Tasty
 import Test.Tasty.HUnit
 
 main :: IO ()
 main = defaultMain $ testGroup ""
-    [ testCase "ok" $
-          return ()
+    [ testCase "repostUpdates" $
+          runTestIO repostUpdates
     ]
+
+data TestIO a = TestIO a deriving Functor
+instance Applicative TestIO where
+    pure = TestIO
+    (TestIO f) <*> tio = fmap f tio
+instance Monad TestIO where
+    (TestIO x) >>= f = f x
+
+runTestIO :: TestIO a -> IO a
+runTestIO (TestIO x) = return x
